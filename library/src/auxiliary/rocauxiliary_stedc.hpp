988d0d915bef5dc47717ed7e02159e25f4b4773f
Ilya Perminov <Ilya.Perminov@amd.com>
2025-08-06 00:39:14 +0200

    Fix compilation issue if slaed4 solver is used

--- library/src/auxiliary/rocauxiliary_stedc.hpp
+++ library/src/auxiliary/rocauxiliary_stedc.hpp
@@ -506,7 +506,7 @@ void __device__ inline solve_seq_eqns(const rocblas_int dd,
                                       const rocblas_int* mask,
                                             S* tmpd,
                                             S* ev,
-                                      const S* zz)
+                                            S* zz)
 {
     /* ----------------------------------------------------------------- */
 
